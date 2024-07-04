7aa52cf72840f3907313bd8ce840d451a0d7af70
Gargi Rout <87861208+gargi-3@users.noreply.github.com>
2024-07-04 12:39:15 +0530

    Update settings.py

--- mybooksproject/mybooksproject/settings.py
+++ mybooksproject/mybooksproject/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s6r!7lghu8m5+mn$os%-i_izwvn8oaj$5=+x%=&!a*9#k%=k-u'
+SECRET_KEY = '' # Your secret key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
